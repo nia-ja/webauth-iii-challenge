fd951b989bce5b803581cdd7aad38d3639b4d537
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-06-13 19:09:52 -0700

    Added getUserById route in usersRouter

--- server/api/routers/usersRouter.js
+++ server/api/routers/usersRouter.js
@@ -1,4 +1,4 @@
-const { getUsers } = require('../helpers');
+const { getUsers, getUserById } = require('../helpers');
 const router = require('express').Router();
 
 const restricted = require('../../auth/restricted-middleware.js');
@@ -15,6 +15,18 @@ router.get('/', restricted, async (req, res) => {
         console.log(err)
         return res.status(500).send(err)
     }
+});
+
+router.get('/:id', restricted, (req, res) => {
+    getUserById(req.params.id)
+      .then(user => {
+        if (user) {
+            res.status(200).json(user);
+          } else {
+            res.status(400).json({ message: "There's no user with this id"})
+          }
+      })
+      .catch(err => res.send(err));
 });
 
 module.exports = router;
